f29a8c776134b747efeccc7dc83d057d7803a93f
bhargavi51 <155128758+bhargavi51@users.noreply.github.com>
2023-12-29 16:30:14 +0530

    Add files via upload

---
+++ QuadraticRoot.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class QuadraticRoot
+
+{
+	
+public static void main(String[] args) 
+{
+		
+Scanner s=new Scanner(System.in);
+	   
+double a,b,c;
+		
+System.out.println("Enter the Year :");
+		a=s.nextDouble();
+		
+b=s.nextDouble();
+		
+c=s.nextDouble();
+		
+double r1,r2,d;
+		
+d=b*b-4*a*c;
+		
+if(d<0)
+		
+{
+		   
+System.out.println("Roots are imaginary"); 
+		}
+		
+else
+		
+{
+		    
+r1=(-b+Math.sqrt(d))/(2*a); 
+		    
+r2=(-b-Math.sqrt(d))/(2*a);
+		   System.out.println("Root1=" +r1);
+		   System.out.println("Root2=" +r2);
+		    }
+	
+}
+
+}
