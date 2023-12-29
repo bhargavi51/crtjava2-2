b6f860d01d4cf11db89e795f6542eb4c866005b7
bhargavi51 <155128758+bhargavi51@users.noreply.github.com>
2023-12-29 15:17:30 +0530

    Add files via upload

---
+++ PositiveNegative.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class PositiveNegative
+{
+	
+public static void main(String[] args) 
+	
+{
+	
+Scanner s=new Scanner(System.in);
+	   
+int a;
+		
+System.out.println("Enter the number :");
+		a=s.nextInt();
+		
+if(a>0)
+		
+{
+		     
+System.out.println("positive number");
+		    }
+		    
+else   if (a==0)
+		    
+{
+		    
+System.out.println("The number is Zero");
+		   }
+		    
+else
+		    
+{
+		      
+System.out.println("negative number");   
+		   }
+		
+		
+}
+
+}
