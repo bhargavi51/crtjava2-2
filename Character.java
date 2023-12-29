ed255b7c5598481fadd7977e5e83f65f961dfcab
bhargavi51 <155128758+bhargavi51@users.noreply.github.com>
2023-12-29 14:57:58 +0530

    Add files via upload

---
+++ Character.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Character
+
+{
+	
+public static void main(String[] args) 
+	
+{
+	
+Scanner s=new Scanner(System.in);
+	   
+char a;
+		
+System.out.println("Enter the Character for a:");
+		a=s.next().charAt(0);
+		
+if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u'||a=='A'||a=='E'||a=='I'||a=='O'||a=='U')
+		
+{
+		     
+System.out.println("Character is Vowel");
+		    }
+		    
+else
+		    
+{
+		    
+System.out.println("Character is Consonent");
+		}
+		
+		
+}
+
+}
