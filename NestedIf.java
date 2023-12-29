5f8e4d83ba80f9c42cfb6dc4a486802268ceccb2
bhargavi51 <155128758+bhargavi51@users.noreply.github.com>
2023-12-29 14:12:34 +0530

    Add files via upload

---
+++ NestedIf.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+public class NestedIf
+{
+	
+public static void main(String[] args) 
+	
+{
+	
+Scanner s=new Scanner(System.in);
+		int a,b,c;
+		
+System.out.println("Enter the values for a,b,c:");
+		a=s.nextInt();
+		
+b=s.nextInt();
+		
+c=s.nextInt();
+		
+if(a>b)
+		
+{
+		    
+if(a>c)
+		    
+{
+		     
+System.out.println(a+"  a is biggest");
+		    }
+		    
+else
+		    
+{
+		    
+System.out.println(c+" c is biggest ");
+		    }
+		
+}
+		
+if(b>c)
+		
+{
+		
+System.out.println(c+" b is biggest");
+		
+}
+		
+else
+		
+{
+		
+System.out.println(c+" c is biggest"); 
+		
+}
+	
+}
+
+}
