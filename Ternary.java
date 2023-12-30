c97da87d40f189b8805f07da44dc70904af0ed33
bhargavi51 <155128758+bhargavi51@users.noreply.github.com>
2023-12-30 12:02:41 +0530

    Add files via upload

---
+++ Ternary.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Ternary
+
+{
+	
+public static void main(String[] args)
+	{
+	
+Scanner s=new Scanner(System.in);
+	int a,b,r;
+	
+a=s.nextInt();
+	
+b=s.nextInt();
+	  
+r=(a>b)?a:b;
+	  
+System.out.println(r);
+	
+}
+
+}
---
+++ Ternary1.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+/* using ternary operator to find smallest among 3 numbers*/
+public class Ternary1
+{
+	
+public static void main(String[] args)
+	{
+	
+Scanner s=new Scanner(System.in);
+	int a,b,c,r;
+	
+a=s.nextInt();
+	
+b=s.nextInt();
+	
+c=s.nextInt();
+	  
+r=(a<b && a<c)?a:(b<c)?b:c;
+	  System.out.println(r);
+	
+}
+
+}
