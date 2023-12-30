97686282825de365c02e2736a8bdd0fcf42afb88
bhargavi51 <155128758+bhargavi51@users.noreply.github.com>
2023-12-30 13:27:35 +0530

    Add files via upload

---
+++ SwitchCase1.java
@@ -0,0 +1,61 @@
+import java.util.*;
+
+public class Main
+
+{
+	
+public static void SwitchCase1(String[] args)
+	
+{
+	
+Scanner s=new Scanner(System.in);
+	
+char a;
+	
+a=s.next().charAt(0);
+	
+switch(a)
+	
+{
+	   
+case 'a':
+	   
+case 'e':
+	   
+case 'i':
+	   
+case 'o':
+	   
+case 'u':
+	   
+case 'A':
+	   
+case 'E':
+	   
+case 'I':
+	   
+case 'O':
+	   
+case 'U':
+	       
+System.out.println("character is vowel");
+	       break;
+	   
+default:
+	        
+System.out.println("gnmj");
+	       break;
+	   
+case 0:
+	       
+System.out.println("Character is Consonant");
+	      
+ break;
+	   
+	
+	
+}
+
+}
+
+}
---
+++ SwitchCase2.java
@@ -0,0 +1,72 @@
+import java.util.*;
+
+public class SwitchCase2
+{
+	
+public static void main(String[] args)
+	
+{
+	
+Scanner s=new Scanner(System.in);
+	
+int a,b,r;
+	
+System.out.println("Enter 2 numbers");
+	
+a=s.nextInt();
+	
+b=s.nextInt();
+	
+System.out.println("1.Addition\n 2.Subtraction\n 3.Multiplication \n 4.Division ");
+	
+System.out.println("Enter your choice");
+	
+int ch;
+	
+ch=s.nextInt();
+	
+switch(ch)
+	
+{
+	   
+case 1:
+	       
+r=a+b;
+	       
+System.out.println("sum  of 2 numbers is:" +r);
+	       
+break;
+	   
+case 2:
+	       
+r=a-b;
+	       
+System.out.println("difference of 2 numbers is:" +r);
+	       break;
+	   	
+case 3:
+	       
+r=a*b;
+	       
+System.out.println("product of 2 numbers is:" +r);
+	       
+break;
+	    
+case 4:
+	       
+r=a/b;
+	       
+System.out.println(" division of 2 numbers is:" +r);
+	       break;
+	    
+default:
+	       
+System.out.println("Enter valid number");
+	   
+	
+	}
+	 
+	
+}
+
+}
