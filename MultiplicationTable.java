dc11a93d1910ef8c6b142f77edb3a8f965bd66d1
bhargavi51 <155128758+bhargavi51@users.noreply.github.com>
2024-01-02 10:02:53 +0530

    Add files via upload

---
+++ MultiplicationTable.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class MultiplicationTable
+/* Multiplication table table using for loop*/
+
+{
+	
+public static void main(String[] args) 
+{
+	
+Scanner s=new Scanner(System.in);
+	
+int i,n;
+	
+n=s.nextInt();
+	
+for(i=1;i<=n;i++)
+	
+{
+	    	
+System.out.println(i+ "*" +n+"="+i*n);
+	
+}
+
+	
+}
+
+}
