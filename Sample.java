4e45a50bf1155c3bde3dbfc892cc64f6dc167460
bhargavi51 <155128758+bhargavi51@users.noreply.github.com>
2024-01-04 10:07:44 +0530

    Add files via upload

---
+++ Sample.java
@@ -0,0 +1,33 @@
+public class Sample
+
+{
+    
+int a,b;
+	
+public static void main(String[] args) 
+	
+{
+	      
+Sample m1=new  Sample();
+	    
+m1.a=10;
+	    
+m1.b=20;
+	     
+Sample m2=new Sample ();
+	    
+m2.a=100;
+	    
+m2.b=200;
+	    
+System.out.println("a="+m1.a+" b= "+m1.b);
+	    
+System.out.println("a="+m1.a+" b= "+m1.b);
+	   
+    
+    
+    
+	
+}
+
+}
